9d6efcc7aa86078b595f4ccc07b8efffbdedf008
Mark Huang <hhzeos@gmail.com>
2020-02-03 09:18:17 +0800

    Update

--- bot/bot_test.py
+++ bot/bot_test.py
@@ -6,7 +6,7 @@ def hello(update, context):
         'Hello {}'.format(update.message.from_user.first_name))
 
 
-updater = Updater('901974141:AAEFcVfZfAIKlKwBvm8aSFKVDsNyRL_Hhk8', use_context=True)
+updater = Updater('API', use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 

bc8d26bb14fe5fe6e4619b678c5deaa5ae8f9d0f
Mark Huang <hhzeos@gmail.com>
2020-09-05 10:40:01 +0800

    Fix missing recurring events

--- src/calendarFixer.py
+++ src/calendarFixer.py
@@ -1,132 +1,180 @@
-import string
 from pathlib import Path
 from typing import List, Dict, Tuple
 from markkk.logger import logger
-
-requiredLines = (
-    "BEGIN:VEVENT",
-    "SUMMARY:",
-    "DTSTART:",
-    "DTEND:",
-    "UID:",
-    "END:VEVENT",
-)
+from pprint import pprint
+from collections import OrderedDict
 
 
-def find_2nd(string: str, substring: str):
-    return string.find(substring, string.find(substring) + 1)
+def fix_broken_lines(event_lines_list: List):
+    keywords = (
+        "BEGIN",
+        "DESCRIPTION",
+        "SUMMARY",
+        "DTSTART",
+        "DTEND",
+        "DTSTAMP",
+        "UID",
+        "CREATED",
+        "LAST-MODIFIED",
+        "SEQUENCE",
+        "STATUS",
+        "RRULE",
+        "EXDATE",
+        "END",
+    )
+    fixed = []
+    for line in event_lines_list:
+        line = line.strip()
+        colon_index = line.find(":")
+        if colon_index != -1:
+            key = line[:colon_index]
+            if key in keywords:
+                fixed.append(line)
+            else:
+                fixed[-1] = fixed[-1] + line
+        else:
+            fixed[-1] = fixed[-1] + line
+    return fixed
 
 
-def modifyEvent(event: list) -> list:
+def get_event_list(ics_path: str) -> List[List[str]]:
+    ics_path = Path(ics_path)
+    if not ics_path.is_file():
+        logger.error("File does not exist.")
 
-    # If SUMMARY break into 2 lines
-    if len(event) == len(requiredLines) + 1:
-        # remove \n for SUMMARY
-        event[1] = event[1][0:-1]
-        # join the second line into SUMMARY
-        event[1] += event[2].strip()
-        # delete second line
-        del event[2]
+    with ics_path.open() as f:
+        lines = f.readlines()
 
-    # find location information at original SUMMARY line
-    location_start = event[1].find("ROOM:")
+    reading_event: bool = False
 
-    # if location info is not found in SUMMARY
-    if location_start == -1:
-        # do nothing
-        pass
-    # location info is found
+    event_list: List[List[str]] = []
+    current_event_lines: List[str] = []
+
+    for line in lines:
+        line: str = line.strip()
+
+        if line.startswith("BEGIN:VEVENT"):
+            reading_event = True
+
+        if reading_event:
+            current_event_lines.append(line)
+
+        if line.startswith("END:VEVENT"):
+            reading_event = False
+            event_list.append(fix_broken_lines(current_event_lines))
+            # reset
+            current_event_lines = []
+
+    return event_list
+
+
+def parse_single_event(event_lines: List[str]) -> Dict[str, str]:
+    if event_lines[0] == "BEGIN:VEVENT" and event_lines[-1] == "END:VEVENT":
+        event_lines = event_lines[1:-1]
     else:
-        # offset start point from before 'ROOM:' to after
-        location_start += 6
-        # create LOCATION line
-        location = "LOCATION:" + event[1][location_start:] + "\n"
-        # insert LOCATION line into the event
-        event.insert(2, location)
+        logger.error("Invalid Event")
+        return
 
-    # determine the string slicing end point at SUMMARY line
-    summary_end = find_2nd(event[1], ":") - 3
-    # only if the slicing point is found
-    if summary_end != -4:
-        # slice the SUMMARY line to keep module name only
-        event[1] = event[1][0:summary_end] + "\n"
+    event = OrderedDict(
+        {
+            "SUMMARY": None,
+            "DESCRIPTION": None,
+            "LOCATION": None,
+            "DTSTART": None,
+            "DTEND": None,
+            "UID": None,
+            "RRULE": None,
+            "EXDATE": None,
+        }
+    )
 
-    # return formatted event
+    for line in event_lines:
+        colon_index = line.find(":")
+        key = line[:colon_index]
+        if key not in event:
+            continue
+        line_value = line[colon_index + 1 :]
+        event[key] = line_value
+
+    # correct summary
+    original_summary = event.get("SUMMARY")
+    logger.info(original_summary)
+    if original_summary:
+        room_index = original_summary.find("ROOM:")
+        if room_index != -1:
+            location_value = original_summary[room_index + 5 :]
+            event["LOCATION"] = location_value.strip()
+            original_summary = original_summary[:room_index]
+
+        chop_index = original_summary.find(":")
+        if chop_index != -1:
+            chopped = original_summary[: chop_index - 2]
+            event["SUMMARY"] = chopped.strip()
+
+    if event.get("SUMMARY") == event.get("DESCRIPTION"):
+        event.pop("DESCRIPTION")
+
+    unused = []
+    for key in event.keys():
+        if not event.get(key):
+            unused.append(key)
+
+    for key in unused:
+        event.pop(key)
+
     return event
 
 
-def fix(file: str) -> Tuple[str, int]:
+def parse_event_list(event_list: List[List[str]]) -> List[Dict]:
+    parsed_event_list = []
+    for event in event_list:
+        parsed_event = parse_single_event(event)
+        parsed_event_list.append(parsed_event)
+    return parsed_event_list
 
-    # open ics file in read mode
-    file = Path(file)
-    if not file.is_file():
-        logger.error(f"Invalid file path: '{file}'")
-        raise Exception(f"Invalid file path: '{file}'")
+
+def generate_new_content_to_write(parsed_event_list: List[Dict]) -> str:
+    lines_to_write: List[str] = []
+    for event in parsed_event_list:
+        start = "BEGIN:VEVENT"
+        lines_to_write.append(start)
+        for key, value in event.items():
+            line = f"{key}:{value}"
+            lines_to_write.append(line)
+        end = "END:VEVENT"
+        lines_to_write.append(end)
+
+    lines_to_write.insert(0, "BEGIN:VCALENDAR")
+    lines_to_write.append("VERSION:2.0\nEND:VCALENDAR")
+
+    string_to_write = "\n".join(lines_to_write)
+    return string_to_write
+
+
+def fix(ics_path: str):
+    ics_path = Path(ics_path)
+    if not ics_path.is_file():
+        logger.error(f"Invalid ics_path path: '{ics_path}'")
+        raise Exception(f"Invalid ics_path path: '{ics_path}'")
     else:
-        file = file.resolve()
+        ics_path = ics_path.resolve()
 
-    if str(file)[-4:] != ".ics":
-        logger.error(f"'{file}' is not an '.ics' file")
-        raise Exception(f"'{file}' is not an '.ics' file")
+    if str(ics_path)[-4:] != ".ics":
+        logger.error(f"'{ics_path}' is not an '.ics' ics_path")
+        raise Exception(f"'{ics_path}' is not an '.ics' ics_path")
 
-    with file.open() as f:
-        original = f.readlines()
+    event_list = get_event_list(ics_path)
+    parsed_event_list = parse_event_list(event_list)
+    new_content = generate_new_content_to_write(parsed_event_list)
 
-    # a large list of events
-    Events = []
-    # a list to store one event as lines of strings
-    event = []
-
-    key_index = 0
-    # traverse original ics file line by line
-    for line in original:
-        # collecting useful lines for one event
-        if requiredLines[key_index] in line:
-            event.append(line)
-            if key_index <= len(requiredLines) - 2:
-                key_index += 1
-            elif key_index == len(requiredLines) - 1:
-                Events.append(event)
-                event = []
-                # reset key_index
-                key_index = 0
-            else:
-                pass
-        elif requiredLines[key_index] not in line and key_index == 2:
-            event.append(line)
-        else:
-            pass
-
-    # New big list of events
-    new_events = []
-
-    # traverse all events
-    for event in Events:
-        # clean up event
-        event = modifyEvent(event)
-        # append to new big list
-        new_events.append(event)
-
-    # # print to verify
-    # for index, event in enumerate(new_events):
-    #     print(f"{index+1}: {event}")
-    # print()
-    # print(f"Total {len(new_events)} events")
-    # print()
-
-    # create the final long string for ics
-    newContent = "BEGIN:VCALENDAR\n"
-    for event in new_events:
-        for line in event:
-            newContent += line
-    newContent += "VERSION:2.0\nEND:VCALENDAR"
-
-    # create a new ics file
-    folder: Path = file.parent
-    export_fp: Path = folder / (file.stem + "_new.ics")
+    # create a new ics ics_path
+    folder: Path = ics_path.parent
+    export_fp: Path = folder / (ics_path.stem + "_new.ics")
     with export_fp.open(mode="w") as f:
-        f.write(newContent)
+        f.write(new_content)
 
     logger.debug(f"Successfully exported: {export_fp}")
 
-    return export_fp, len(new_events)
+
+if __name__ == "__main__":
+    fix("tests/resources/original_ics/term4.ics")
